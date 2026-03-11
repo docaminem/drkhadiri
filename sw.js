2f53efe85c6f75a27f0e79f695a624ad34edb791
docaminem <doc.khadiriamine@gmail.com>
2026-03-11 17:02:49 +0000

    feat: create PWA demo for Sintrom dosage and INR tracking

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "avk-care-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          if (event.request.method === "GET" && response.ok && event.request.url.startsWith("http")) {
+            const clone = response.clone();
+            caches.open(CACHE).then((cache) => cache.put(event.request, clone));
+          }
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
